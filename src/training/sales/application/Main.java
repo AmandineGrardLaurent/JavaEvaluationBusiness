a84e548cbfa41a510c4c751c0da1280f37d53953
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-06 14:51:20 +0100

    feat: add user input handling methods to Main - Add askChoice() to display options and get user input - Add askKeyword() to prompt for keyword input - Use a switch-case structure to handle user choices

--- src/training/sales/application/Main.java
+++ src/training/sales/application/Main.java
@@ -1,6 +1,7 @@
 package training.sales.application;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
@@ -15,22 +16,57 @@ public class Main {
 	    System.out.println("\n");
 	}
 	
+	public static int askChoice(Scanner scan) {
+		int number;
+		
+		while (true) {
+			System.out.println("Quel est votre choix ? ");
+			System.out.println("[1] Afficher la liste de toutes les formations\n"
+					+ "[2] Afficher les formations en présentiel\n"
+					+ "[3] Afficher les formations en ligne\n"
+					+ "[4] Afficher les formations mixtes\n"
+					+ "[5] Rechercher les formations avec un mot-clé");
+			
+			// Check if the next token is an integer
+			if (!scan.hasNextInt()) {
+				System.out.println("Veuillez entrer un nombre entier.");
+				scan.next();
+				continue;
+			}
+			
+			number = scan.nextInt();
+			break;
+		}
+		return number;
+	}
+	
+	public static String askKeyword(Scanner scan) {
+		System.out.println("Saisissez le mot-clé : ");
+		return scan.next().toLowerCase().trim();
+	}
+	
 	public static void main(String[] args) {
-
-		// Display all courses
-		displayCourses(Course.getAllCourses(), "La liste des formations : ");
-	    
-	    // Display courses on-site only
-	    displayCourses(Course.getCoursesByType(true, false), "La liste des formations en présentiel : ");
-	    
-	    // Display courses online only
-	    displayCourses(Course.getCoursesByType(false, true), "La liste des formations en ligne : ");
-	    
-	    // Display courses available in both types
-	    displayCourses(Course.getCoursesByType(true, true), "La liste des formations disponibles en ligne et en présentiel : ");
-
-	    // Display courses contain a keyword
-	    String keyword = "API";
-	    displayCourses(Course.getCoursesByKeyword(keyword), String.format("La liste des formations correspondant au mot-clé %s : ", keyword));
+		
+		Scanner scan = new Scanner(System.in);
+		int choice = askChoice(scan);
+		
+		switch(choice) {
+			case 1 : 
+				// Display all courses
+				displayCourses(Course.getAllCourses(), "La liste des formations : ");
+			case 2 : 
+				// Display courses on-site only
+				displayCourses(Course.getCoursesByType(true, false), "La liste des formations en présentiel : ");
+			case 3 : 	
+				// Display courses online only
+				displayCourses(Course.getCoursesByType(false, true), "La liste des formations en ligne : ");
+			case 4 : 
+			    // Display courses available in both types
+			    displayCourses(Course.getCoursesByType(true, true), "La liste des formations disponibles en ligne et en présentiel : ");
+			case 5 :
+			    // Display courses contain a keyword
+			    String keyword = askKeyword(scan);
+			    displayCourses(Course.getCoursesByKeyword(keyword), String.format("La liste des formations correspondant au mot-clé %s : ", keyword));	
+		}
 	}
 }
