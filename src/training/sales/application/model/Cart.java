283929f3f195a52a0b30777c3b33df9ae3f77f9a
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-06 18:24:57 +0100

    feat: add User model class - Add User.java to the model package - Add getters and setters for all attributes

---
+++ src/training/sales/application/model/Cart.java
@@ -0,0 +1,17 @@
+package training.sales.application.model;
+
+public class Cart {
+	private int id;
+	
+	public Cart(int id) {
+		this.setId(id);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
