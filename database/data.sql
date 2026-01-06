6de6bc440044e75a6b32dd075ef4f35a402503d0
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-06 09:01:18 +0100

    feat: Created data.sql file to insert initial data into the courses table.

---
+++ database/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO Course (name, description, duration_days, is_presential, is_online, price) VALUES
+('Java Débutant',
+ 'Bases du langage Java et programmation orientée objet',
+ 5, TRUE, TRUE, 1200.00),
+
+('Java Avancé',
+ 'Approfondissement Java, collections, streams et bonnes pratiques',
+ 4, TRUE, FALSE, 1500.00),
+
+('UML et Modélisation',
+ 'Diagrammes UML : cas d’utilisation, classes et séquences',
+ 3, FALSE, TRUE, 900.00),
+
+('Base de données relationnelles',
+ 'Conception MCD, MLD et SQL avec MySQL/MariaDB',
+ 4, TRUE, TRUE, 1100.00),
+
+('Spring Boot',
+ 'Création d’API REST avec Spring Boot',
+ 5, FALSE, TRUE, 1800.00),
+
+('Git et gestion de versions',
+ 'Utilisation de Git et GitHub en équipe',
+ 2, TRUE, TRUE, 600.00);
