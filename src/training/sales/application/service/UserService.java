479f460641b24d7b7e7c349a00dc382dbbae79b7
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 15:14:23 +0100

    feat: add Javadoc comments in UserService

--- src/training/sales/application/service/UserService.java
+++ src/training/sales/application/service/UserService.java
@@ -3,11 +3,23 @@ package training.sales.application.service;
 import training.sales.application.dao.UserDAO;
 import training.sales.application.model.User;
 
-
+/**
+ * Service class for managing user-related operations.
+ * Provides methods to authenticate users and retrieve user information.
+ */
 public class UserService {
-	private UserDAO userDao = new UserDAO();
-	
-	public User getUserByLoginPassword(String login, String password){
-		return userDao.readByLoginPassword(login, password);
-	}
+
+    private UserDAO userDao = new UserDAO();
+
+    /**
+     * Retrieves a User by their login and password.
+     * This is typically used for authentication purposes.
+     *
+     * @param login the user's login
+     * @param password the user's password
+     * @return a User object if credentials are correct, null otherwise
+     */
+    public User getUserByLoginPassword(String login, String password) {
+        return userDao.readByLoginPassword(login, password);
+    }
 }
