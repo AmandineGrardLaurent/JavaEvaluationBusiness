dcd72414857d537c3db35aa0e9148647eb320a7b
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 14:48:39 +0100

    feat: add Javadoc comments in DatabaseConnection

--- src/training/sales/application/dao/DatabaseConnection.java
+++ src/training/sales/application/dao/DatabaseConnection.java
@@ -7,12 +7,19 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Utility class to manage database connections.
+ * 
+ * Loads database configuration from the config.properties file
+ * and provides a method to obtain a JDBC Connection.
+ */
 public class DatabaseConnection {
 
     private static String URL;
     private static String USER;
     private static String PASSWORD;
 
+    // Static block to load database configuration from properties file
     static {
         Properties prop = new Properties();
         try (FileInputStream fis = new FileInputStream("config.properties")) {
@@ -22,10 +29,16 @@ public class DatabaseConnection {
             PASSWORD = prop.getProperty("db.password");
         } catch (IOException e) {
             e.printStackTrace();
-            throw new RuntimeException("Impossible de charger le fichier config.properties");
+            throw new RuntimeException("Unable to load config.properties file");
         }
     }
 
+    /**
+     * Returns a new JDBC connection to the database.
+     *
+     * @return a Connection object
+     * @throws SQLException if a database access error occurs
+     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
