ab46fabdb4982ca8ce2533f80682cb1587c79a95
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 15:04:23 +0100

    feat: add Javadoc comments in Order model class

--- src/training/sales/application/model/Order.java
+++ src/training/sales/application/model/Order.java
@@ -2,17 +2,30 @@ package training.sales.application.model;
 
 import java.util.Date;
 
+/**
+ * Represents an order in the application.
+ * An order has an ID, a date, and a total amount.
+ */
 public class Order {
-	
-	private int id;
-	private Date orderDate;
-	private double totalAmount;
-	
-	public Order(int id, Date orderDate, double totalAmount) {
-		this.id = id;
-		this.orderDate = orderDate;
-		this.totalAmount = totalAmount;
-	}
+
+    private int id;
+    private Date orderDate;
+    private double totalAmount;
+
+    /**
+     * Constructs an Order with the specified attributes.
+     *
+     * @param id the unique identifier of the order
+     * @param orderDate the date when the order was placed
+     * @param totalAmount the total monetary amount of the order
+     */
+    public Order(int id, Date orderDate, double totalAmount) {
+        this.id = id;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    // Getters and setters
 
 	public int getId() {
 		return id;
