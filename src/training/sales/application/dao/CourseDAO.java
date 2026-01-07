6c31b60e58bfbc45fd4888941227e7500e4e045a
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 14:45:27 +0100

    feat: add Javadoc comments in CourseDAO

--- src/training/sales/application/dao/CourseDAO.java
+++ src/training/sales/application/dao/CourseDAO.java
@@ -9,10 +9,18 @@ import java.util.List;
 
 import training.sales.application.model.Course;
 
-
+/**
+ * Data Access Object for the Course entity.
+ * Handles database operations related to Course objects.
+ */
 public class CourseDAO {
 
-	public List<Course> readAll() {
+    /**
+     * Retrieves all courses from the database.
+     *
+     * @return a list of all Course objects
+     */
+    public List<Course> readAll() {
         List<Course> courses = new ArrayList<>();
         String sql = "SELECT id_course, name, description, duration_days, is_on_site, is_online, price FROM Course";
 
@@ -40,29 +48,36 @@ public class CourseDAO {
         return courses;
     }
 	
-	public List<Course> readAllByType(boolean onSite, boolean online){
-		List<Course> courses = new ArrayList<>();
+    /**
+     * Retrieves courses filtered by type (on-site and/or online).
+     *
+     * @param onSite  whether the course is available on-site
+     * @param online  whether the course is available online
+     * @return a list of Course objects matching the type filters
+     */
+    public List<Course> readAllByType(boolean onSite, boolean online) {
+        List<Course> courses = new ArrayList<>();
         String sql = "SELECT id_course, name, description, duration_days, is_on_site, is_online, price FROM Course WHERE is_on_site = ? AND is_online = ?";
 
         try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement preparedStmt = connection.prepareStatement(sql)){
-            	 preparedStmt.setBoolean(1, onSite);
-            	 preparedStmt.setBoolean(2, online);    
-        		
-        try (ResultSet resultSet = preparedStmt.executeQuery()){
+             PreparedStatement preparedStmt = connection.prepareStatement(sql)) {
 
-	            while (resultSet.next()) {
-	                int id = resultSet.getInt("id_course");
-	                String name = resultSet.getString("name");
-	                String description = resultSet.getString("description");
-	                int durationDays = resultSet.getInt("duration_days");
-	                boolean isOnSite = resultSet.getBoolean("is_on_site");
-	                boolean isOnline = resultSet.getBoolean("is_online");		
-	                double price = resultSet.getDouble("price");
-	
-	                Course course = new Course(id, name, description, durationDays, isOnSite, isOnline, price);
-	                courses.add(course);
-	            }
+            preparedStmt.setBoolean(1, onSite);
+            preparedStmt.setBoolean(2, online);    
+        		
+            try (ResultSet resultSet = preparedStmt.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id_course");
+                    String name = resultSet.getString("name");
+                    String description = resultSet.getString("description");
+                    int durationDays = resultSet.getInt("duration_days");
+                    boolean isOnSite = resultSet.getBoolean("is_on_site");
+                    boolean isOnline = resultSet.getBoolean("is_online");		
+                    double price = resultSet.getDouble("price");
+
+                    Course course = new Course(id, name, description, durationDays, isOnSite, isOnline, price);
+                    courses.add(course);
+                }
             }
 
         } catch (SQLException e) {
@@ -70,31 +85,37 @@ public class CourseDAO {
         }
 
         return courses;
-	}
+    }
 	
-	public List<Course> readAllByKeyword(String keyword){
-		List<Course> courses = new ArrayList<>();
+    /**
+     * Retrieves courses whose name or description contains the specified keyword.
+     *
+     * @param keyword the search keyword
+     * @return a list of Course objects that match the keyword
+     */
+    public List<Course> readAllByKeyword(String keyword) {
+        List<Course> courses = new ArrayList<>();
         String sql = "SELECT id_course, name, description, duration_days, is_on_site, is_online, price FROM Course WHERE name LIKE ? OR description LIKE ?";
 
         try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement preparedStmt = connection.prepareStatement(sql)){
-            	 preparedStmt.setString(1, "%" + keyword + "%");
-            	 preparedStmt.setString(2, "%" + keyword + "%");
-        		
-        try (ResultSet resultSet = preparedStmt.executeQuery()){
+             PreparedStatement preparedStmt = connection.prepareStatement(sql)) {
 
-	            while (resultSet.next()) {
-	                int id = resultSet.getInt("id_course");
-	                String name = resultSet.getString("name");
-	                String description = resultSet.getString("description");
-	                int durationDays = resultSet.getInt("duration_days");
-	                boolean isOnSite = resultSet.getBoolean("is_on_site");
-	                boolean isOnline = resultSet.getBoolean("is_online");		
-	                double price = resultSet.getDouble("price");
-	
-	                Course course = new Course(id, name, description, durationDays, isOnSite, isOnline, price);
-	                courses.add(course);
-	            }
+            preparedStmt.setString(1, "%" + keyword + "%");
+            preparedStmt.setString(2, "%" + keyword + "%");
+        		
+            try (ResultSet resultSet = preparedStmt.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id_course");
+                    String name = resultSet.getString("name");
+                    String description = resultSet.getString("description");
+                    int durationDays = resultSet.getInt("duration_days");
+                    boolean isOnSite = resultSet.getBoolean("is_on_site");
+                    boolean isOnline = resultSet.getBoolean("is_online");		
+                    double price = resultSet.getDouble("price");
+
+                    Course course = new Course(id, name, description, durationDays, isOnSite, isOnline, price);
+                    courses.add(course);
+                }
             }
 
         } catch (SQLException e) {
@@ -102,6 +123,6 @@ public class CourseDAO {
         }
 
         return courses;
-	}
+    }
 	
 }
