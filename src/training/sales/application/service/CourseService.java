1e53f4c8ecb34179a5f702b4b7405ccfd72768d9
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 15:11:55 +0100

    feat: add Javadoc comments in CourseService

--- src/training/sales/application/service/CourseService.java
+++ src/training/sales/application/service/CourseService.java
@@ -5,20 +5,41 @@ import java.util.List;
 import training.sales.application.dao.CourseDAO;
 import training.sales.application.model.Course;
 
+/**
+ * Service class for managing course-related operations.
+ * Provides methods to retrieve courses from the database.
+ */
 public class CourseService {
-	
-	private CourseDAO courseDao = new CourseDAO();
-	
 
-	public List<Course> getAllCourses() {	
-		return courseDao.readAll();
-	}
-	
-	public List<Course> getCoursesByType(boolean isOnSite, boolean isOnline){
-		return courseDao.readAllByType(isOnSite, isOnline);
-	}
-	
-	public List<Course> getCoursesByKeyword(String keyword){
-		return courseDao.readAllByKeyword(keyword);
-	}
+    private CourseDAO courseDao = new CourseDAO();
+
+    /**
+     * Retrieves all courses from the database.
+     *
+     * @return a list of all Course objects
+     */
+    public List<Course> getAllCourses() {
+        return courseDao.readAll();
+    }
+
+    /**
+     * Retrieves courses filtered by type (on-site, online, or both).
+     *
+     * @param isOnSite true if the course is on-site
+     * @param isOnline true if the course is online
+     * @return a list of Course objects matching the type
+     */
+    public List<Course> getCoursesByType(boolean isOnSite, boolean isOnline) {
+        return courseDao.readAllByType(isOnSite, isOnline);
+    }
+
+    /**
+     * Retrieves courses that match a keyword in their name or description.
+     *
+     * @param keyword the keyword to search for
+     * @return a list of Course objects matching the keyword
+     */
+    public List<Course> getCoursesByKeyword(String keyword) {
+        return courseDao.readAllByKeyword(keyword);
+    }
 }
