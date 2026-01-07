284b1f812bea71a992363660cef5afa225f8e8c9
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 14:56:34 +0100

    feat: add Javadoc comments in Cart.java

--- src/training/sales/application/model/Cart.java
+++ src/training/sales/application/model/Cart.java
@@ -1,38 +1,50 @@
 package training.sales.application.model;
 
+/**
+ * Represents a shopping cart item linking a user to a course.
+ * Each Cart object corresponds to a course added by a user in their cart.
+ */
 public class Cart {
-	private int id;
-	private int idUser;
-	private int idCourse;
-	
-	public Cart(int idUser, int idCourse) {
-		this.idUser = idUser;
-		this.idCourse = idCourse;
-	}
+    
+    private int id;
+    private int idUser;
+    private int idCourse;
+    
+    /**
+     * Constructs a Cart item with the specified user ID and course ID.
+     * The cart ID will typically be assigned by the database.
+     *
+     * @param idUser the ID of the user adding the course
+     * @param idCourse the ID of the course being added
+     */
+    public Cart(int idUser, int idCourse) {
+        this.idUser = idUser;
+        this.idCourse = idCourse;
+    }
 
-	public int getId() {
-		return id;
-	}
+    // getters and setters
+    
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int getIdUser() {
-		return idUser;
-	}
+    public int getIdUser() {
+        return idUser;
+    }
 
-	public void setIdUser(int idUser) {
-		this.idUser = idUser;
-	}
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
 
-	public int getIdCourse() {
-		return idCourse;
-	}
+    public int getIdCourse() {
+        return idCourse;
+    }
 
-	public void setIdCourse(int idCourse) {
-		this.idCourse = idCourse;
-	}
-
-	
+    public void setIdCourse(int idCourse) {
+        this.idCourse = idCourse;
+    }
 }
