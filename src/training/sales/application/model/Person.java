0da20c01aa262a1ef609f1665a822d65c2b704df
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-07 15:05:47 +0100

    feat: add Javadoc comments in Person model class

--- src/training/sales/application/model/Person.java
+++ src/training/sales/application/model/Person.java
@@ -1,15 +1,27 @@
 package training.sales.application.model;
 
+/**
+ * Abstract class representing a person.
+ * Contains basic personal information such as first name and last name.
+ */
 public abstract class Person {
-	private String lastname;
-		private String firstname;
-	
-	public Person(String lastname, String firstname) {
-		this.firstname = firstname;
-		this.lastname = lastname;
-		
-	}
-	
+
+    private String lastname;
+    private String firstname;
+
+    /**
+     * Constructs a Person with the specified last name and first name.
+     *
+     * @param lastname  the last name of the person
+     * @param firstname the first name of the person
+     */
+    public Person(String lastname, String firstname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    // Getters and setters omitted for brevity
+
 	public String getLastname() {
 		return lastname;
 	}
@@ -26,9 +38,13 @@ public abstract class Person {
 		this.firstname = firstname;
 	}
 
-	@Override
-	public String toString() {
-		return getLastname() + " " + getFirstname();
-	}
-	
+    /**
+     * Returns the full name of the person in the format "Lastname Firstname".
+     *
+     * @return the full name
+     */
+    @Override
+    public String toString() {
+        return getLastname() + " " + getFirstname();
+    }
 }
