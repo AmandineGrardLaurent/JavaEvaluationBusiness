1f11a38a2e8e4716023de2d620fdb424f1cee66d
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-01-08 14:40:40 +0100

    feat: add comments javadoc in orderService

--- src/training/sales/application/service/OrderService.java
+++ src/training/sales/application/service/OrderService.java
@@ -5,11 +5,22 @@ import java.util.List;
 import training.sales.application.dao.OrderDAO;
 import training.sales.application.model.Order;
 
-
+/**
+ * Service class for managing order-related business logic.
+ * Provides methods to retrieve orders for a specific user.
+ * Acts as an intermediary between the DAO layer and the presentation layer.
+ */
 public class OrderService {
-	private OrderDAO orderDao = new OrderDAO();
-	
-	public List<Order> getOrderByUser(int idUser) {
+
+    private OrderDAO orderDao = new OrderDAO();
+
+    /**
+     * Retrieves all orders associated with a given user ID.
+     *
+     * @param idUser the ID of the user
+     * @return a list of Order objects for the specified user
+     */
+    public List<Order> getOrderByUser(int idUser) {
         return orderDao.readOrderByUser(idUser);
     }
 }
